a9f04ae638f1a37c9ee4e07c368d4ea776275538
Jon B. Robeson <jj.rrobeson@gmail.com>
2021-12-02 14:33:07 -0600

    display evens code practice

--- classwork/javascript-assignments/display-evens/index.js
+++ classwork/javascript-assignments/display-evens/index.js
@@ -13,6 +13,7 @@ const evens = (e) => {
   let eValue = parseInt(end.value, 10);
   let stepValue = parseInt(step.value, 10);
   let entries = [sValue, eValue, stepValue];
+  let collector = [];
 
   if (!form.checkValidity()) {
     form.className = "was-validated";
@@ -24,7 +25,14 @@ const evens = (e) => {
     return false;
   }
 
-  console.log(entries);
+  for (let i = sValue; i < eValue; i += stepValue) {
+    collector.push(i);
+  }
+
+  let evenValues = collector.filter((value, i) => value % 2 === 0);
+  even.innerText = evenValues.join(" ");
+  console.log("Entries", entries);
+  console.log("even values", evenValues);
 };
 
 button.addEventListener("click", evens);
